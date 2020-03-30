d7333c5d5af1a859a52b08470430ab175b6e9cf8
ste-xx <stefan.breitenstein@gmail.com>
2020-03-30 23:23:06 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const query = require('./query');
 (async () => {
   const client = await createApolloClient();
   const result = await client.query({query, fetchPolicy: 'network-only'});
-
-  console.warn(JSON.stringify(result.data, null, 2));
+  const pretty = result.data.search.edges.map(r => {
+    return {
+      name: `${r.node.owner.url}/${r.node.name}`,
+      description: r.node.descriptionHTML,
+      stargazers: r.node.stargazers.totalCount,
+      cursor: r.cursor
+    }
+  });
+  console.log(result.data.search.repositoryCount);
+  // console.warn(JSON.stringify(result.data, null, 2));
+  console.log(JSON.stringify(pretty, null, 2));
 })();
